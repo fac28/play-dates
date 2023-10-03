af1f8a140d1890710fd3f7fada2712ddf7bf327a
nichgalzin <nichgalzin@gmail.com>
2023-10-03 14:29:46 +0100

    Home layout and route
    Relates #7

--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ const staticHandler = express.static('public');
 const bodyParser = require('body-parser');
 
 const app = express();
+const homeRoutes = require('./routes/home');
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(staticHandler);
 
 //Routes
-
+app.use('/', homeRoutes);
 module.exports = app;
