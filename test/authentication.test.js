c622d7c7596cf55db50f3a2eceac052fc276f892
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-10-03 17:51:21 +0100

    added tests for event models
    Co-authored-by: Yuqing Wang <yuqingwwang@users.noreply.github.com>

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,14 +1,14 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const session = require('../src/model/session.js');
+const session = require('../src/model/sessions.js');
 const users = require('../src/model/users.js');
 const helpers = require('./helpers.js');
 
 test('User can sign up', async () => {
-  const user = await users.createUser('hello@hey.com', 'password')
-  
-  
-})
+  const user = await users.createUser('hello@hey.com', 'password');
+
+//   assert.equal(3, 3);
+});
 
 // test('User can login', async () => {
 // })
