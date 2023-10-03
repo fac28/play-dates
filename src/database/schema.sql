8a114107b07bf537f3ec0b0635e22464b64509fd
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-10-03 12:03:12 +0100

    seed db and fix schema users/user issue

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys = ON;
 
-BEGIN
+BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,15 +18,15 @@ CREATE TABLE IF NOT EXISTS sessions (
 
 CREATE TABLE IF NOT EXISTS events (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER REFERENCES user(id),
-  content TEXT
+  user_id INTEGER REFERENCES users(id),
+  content TEXT,
   event_date DATETIME NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE IF NOT EXISTS partners (
-  user_id INTEGER REFERENCES user(id),
-  partners_id INTEGER REFERENCES user(id),
+  user_id INTEGER REFERENCES users(id),
+  partners_id INTEGER REFERENCES users(id),
   PRIMARY KEY (user_id, partners_id)
 );
 
