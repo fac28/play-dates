6a6faceb8cbe9cf0803162658bb4b8b84855a17a
JamesESS <jesandfordsmith@gmail.com>
2023-10-03 18:13:52 +0100

    re added layout function that was wiped in last merge

--- src/templates/form.js
+++ src/templates/form.js
@@ -23,4 +23,22 @@ function inputForm(events) {
 `;
 }
 
+function layout({ title, content }) {
+  return /*html*/ `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>${title}</title>
+      </head>
+      <body>
+          <main>
+            ${content}
+          </main>
+        </div>
+      </body>
+    </html>
+  `;
+}
+
 module.exports = { inputForm, layout };
