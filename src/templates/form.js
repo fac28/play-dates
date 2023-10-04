62dbf52871dbfd97b90f6d44e592921a5fff553c
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-04 10:53:43 +0100

    Update form.js

--- src/templates/form.js
+++ src/templates/form.js
@@ -23,22 +23,4 @@ function inputForm(events) {
 `;
 }
 
-function layout({ title, content }) {
-  return /*html*/ `
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <title>${title}</title>
-      </head>
-      <body>
-          <main>
-            ${content}
-          </main>
-        </div>
-      </body>
-    </html>
-  `;
-}
-
-module.exports = { inputForm, layout };
+module.exports = { inputForm };
