7d7e753120870c97d7cb7bb1d735793597606f84
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-04 12:53:31 +0100

    temp fix for user_id not defined

--- src/routes/home.js
+++ src/routes/home.js
@@ -12,7 +12,10 @@ router.get('/', (req, res) => {
     const month = 9;
     const sid = req.signedCookies.sid;
     const active_session = session.getSession(sid);
-    const all_events = events.listEvents(active_session.user_id);
+
+    // remove for production: set user_id to be 1 if active_session is undefined
+    const user_id = active_session ? active_session.user_id : 1;
+    const all_events = events.listEvents(user_id);
 
     res.send(layout(year, month, all_events));
   } catch (error) {
