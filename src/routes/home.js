2c3f1057ce6d4afd7a16423c8add49ad47e74337
James Sandford Smith <jesandfordsmith@gmail.com>
2023-10-04 16:22:07 +0100

    linked home and month routes to events model

--- src/routes/home.js
+++ src/routes/home.js
@@ -19,8 +19,8 @@ router.get('/', (req, res) => {
 
     // remove for production: set user_id to be 1 if active_session is undefined
     const user_id = active_session ? active_session.user_id : 1;
-    const all_events = events.listEvents(user_id);
-
+    const all_events = events.listEventsByMonth(user_id, month);
+    console.log(all_events);
     res.send(layout(year, month, all_events));
   } catch (error) {
     console.error('Error with route:', error.message);
