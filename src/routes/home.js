a9ef99144678536a25c46e6c2e4fcc0905d333d6
ShaughnAnderson94 <andersji94@gmail.com>
2023-10-04 12:05:39 +0100

    form adds to calendar

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,38 +1,20 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
+const events = require('../model/events');
 //Variables
 const layout = require('../templates/layout-main');
+const session = require('../model/sessions');
 
 router.get('/', (req, res) => {
   try {
     const year = 2023;
-    const month = 3;
-    const events = [
-      {
-        id: 1,
-        day: 1,
-        title: 'Cook-a-thon',
-      },
-      {
-        id: 2,
-        day: 1,
-        title: 'Bike race',
-      },
-      {
-        id: 3,
-        day: 2,
-        title: 'Bike race',
-      },
-      {
-        id: 4,
-        day: 6,
-        title: 'Bike race',
-      },
-    ];
+    const month = 9;
+    const sid = req.signedCookies.sid;
+    const active_session = session.getSession(sid);
+    const all_events = events.listEvents(active_session.user_id);
 
-    res.send(layout(year, month, events));
+    res.send(layout(year, month, all_events));
   } catch (error) {
     console.error('Error with route:', error.message);
     throw error;
