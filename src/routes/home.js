6b9d4d263cfebed61d1fb1058ce59a4c11ce7cd2
James Sandford Smith <jesandfordsmith@gmail.com>
2023-10-05 11:39:21 +0100

    added partners model. Schema hash changed

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,28 +2,28 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const events = require('../model/events');
+const partners = require('../model/partners');
 //Variables
 const layout = require('../templates/layout-main');
 const session = require('../model/sessions');
 
 router.get('/', (req, res) => {
   try {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const year = currentYear;
-    const currentMonth = currentDate.getMonth();
-    const month = currentMonth;
-
     const sid = req.signedCookies.sid;
     const active_session = session.getSession(sid);
     if (!active_session) {
       return res.redirect('/login');
     }
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
     // remove for production: set user_id to be 1 if active_session is undefined
     const user_id = active_session ? active_session.user_id : 1;
-    const all_events = events.listEventsByMonth(user_id, month);
+    const usersEvents = events.listEventsByMonth(user_id, month);
+    const partnersEvents = partners.listPartnersEvents(user_id, month);
+    const all_events = usersEvents.concat(partnersEvents);
     console.log(all_events);
-    res.send(layout(year, month, all_events));
+    return res.send(layout(year, month, all_events));
   } catch (error) {
     console.error('Error with route:', error.message);
     throw error;
